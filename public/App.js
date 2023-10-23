e9bbc4fa6e1be7f66e4de0d29abb8ce1b7f9683a
Jan Ostrowski/jan <kowal2001.jk@gmail.com>
2023-10-23 11:14:10 +0200

    front-end

---
+++ public/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="baner">
+        
+      </header>
+
+<div className="newuser">
+   
+</div>
+
+      <section className="main">
+
+      </section>
+
+      <footer className="stopka">
+        
+      </footer>
+
+
+    </div>
+  );
+}
+
+export default App;
