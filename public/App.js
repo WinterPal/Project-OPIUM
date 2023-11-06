4575dbb0ed589b61d52a3a49bd9ed9349be065d7
Jasiek121 <145430021+Jasiek121@users.noreply.github.com>
2023-11-06 11:13:40 +0100

    Add files via upload

--- public/App.js
+++ public/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import Reg from './Reg';
 
 
 function App() {
   return (
     <div className="App">
       <header className="baner">
-        
+        <div className="uno">Copium</div>
+        <div className="dos"><input type="text" placeholder="Czego szukasz?" id="hlp" /></div>
+
+
       </header>
 
-<div className="newuser">
-   
-</div>
-
       <section className="main">
-
+      <div className="sld">
+       <h1>Jesteś nowy?</h1> <br></br><a href="Reg.js" id="dd">Zarejestrój się!</a>
+      </div>
       </section>
 
       <footer className="stopka">
-        
+
       </footer>
 
 
