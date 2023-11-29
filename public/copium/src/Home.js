1cd8814762a3d6c1cb34cd25767d067fad02efbe
WinterGuy <szubryt.t@gmail.com>
2023-11-29 10:33:52 +0100

    update

--- public/copium/src/Home.js
+++ public/copium/src/Home.js
@@ -1,63 +1,58 @@
-import ReactDOM from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 
 
 
 function Home() {
   return (
-     <div className="App">
+    <div className="App">
 
-       <header className="baner">
-
-         <div className="uno">Copium</div>
+      <header className="baner">
+        <div className="uno">Copium</div>
         <div className="dos"><input type="text" placeholder="Czego szukasz?" id="hlp" /></div>
-
-
         <div id="user2">
-    <Link to="user">User</Link>
-    </div>
-       </header>
+          <Link to="user">User</Link>
+        </div>
+      </header>
 
 
-       <section className="main">
+      <section className="main">
 
-         <div className="sld">
-           <h1>Jesteś nowy?</h1> <br></br>
+        <div className="sld">
+          <h1>Jesteś nowy?</h1> <br></br>
           <a className="dd">Aby utworzc konto</a> <Link to="/Reg" id="t1">kliknij w link<a className="dd"></a></Link>
         </div>
 
         <div id="ogl">
-         <h7>Ogloszenia</h7>
+          <h7>Ogloszenia</h7>
         </div>
         <div id="auk">
-        <h7>Aukcje</h7>
+          <h7>Aukcje</h7>
         </div>
 
-      <div id="linia2"></div>
+        <div id="linia2"></div>
 
-     <div id="zdj"><img src="logo.png" id="p1"></img></div> 
+        <div id="zdj"><img src="logo.png" id="p1"></img></div>
 
-      <h6>mega ufolud kupuj teraz</h6>
-      <div id="linia3"></div>
-    <div id="cena"><h5>2000zł</h5></div>
+        <h6>mega ufolud kupuj teraz</h6>
+        <div id="linia3"></div>
+        <div id="cena"><h5>2000zł</h5></div>
 
 
-      <div id="linia2"></div>
+        <div id="linia2"></div>
 
-<div id="zdj"><img src="logo.png" id="p1"></img></div> 
+        <div id="zdj"><img src="logo.png" id="p1"></img></div>
 
- <h6>mega ufolud kupuj teraz</h6>
- <div id="linia3"></div>
+        <h6>mega ufolud kupuj teraz</h6>
+        <div id="linia3"></div>
 
       </section>
-      
 
-       <footer className="stopka">
 
-       </footer>
+      <footer className="stopka">
 
-     </div>
-    
+      </footer>
+
+    </div>
   );
 }
 
